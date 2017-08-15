9ab0a7a2701c60fdc58dff65b13695e250e59e9b
Mark Ayers <markthema3@gmail.com>
2017-08-15 12:51:16 -0700

    initial commit

---
+++ config.h
@@ -0,0 +1,13 @@
+#define UserName "demopaasmer@gmail.com"
+#define DeviceName "mark-pi9"
+#define timePeriod 5
+
+char* feedname[] = {"a"};
+
+char* feedtype[] = {"sensor"};
+
+int feedpin[] = {"3"};
+
+char* connectiontype[] = {"GPIO"};
+
+//#define port "/dev/ttyUSB0" // uncomment this line and edit with the USB port if you are using Zigbee
